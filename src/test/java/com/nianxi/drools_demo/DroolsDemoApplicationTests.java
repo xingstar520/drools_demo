d1e633f914ecc52f80d44643720444669f73b3ac
xingstar <2115438238@qq.com>
2024-08-13 14:20:07 +0800

    Initial commit

---
+++ src/test/java/com/nianxi/drools_demo/DroolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nianxi.drools_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
